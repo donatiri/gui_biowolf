02b13a68fbe378de758531c197d8b8fac287ec27
mellowdon <donatiriccardo94@gmail.com>
2021-08-31 14:56:02 +0200

    240bytes
    240 bytes

---
+++ gui_cannibalized_24_08_scrolling/matlab cannibal/viewDATA.m
@@ -0,0 +1,16 @@
+close all
+clear
+clc
+
+while 1
+    [baseName, folder] = uigetfile('.hdf');
+    if baseName == 0
+        break
+    end
+    fullFileName = fullfile(folder, baseName);
+    hdf = h5read(fullFileName,'/dataGroup/dataTable');
+    data = hdf.out;
+    figure(1)
+    %plot(data(:,1));
+    pause()
+end
